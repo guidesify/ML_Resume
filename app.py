e96a4ec6ba7b1476a25a1db2af7f65c66951ff0a
guidesify <64188976+guidesify@users.noreply.github.com>
2022-10-28 16:46:49 +0800

    Separate Pages

--- app.py
+++ app.py
@@ -57,8 +57,62 @@ def show_dataframe():
     fields = list(map(lambda x: x.title() if x not in  ['HR','BPO'] else x.upper(), df.columns))
     return df, fields
 
-# App UI
-if __name__ == '__main__':
+def page_one():
+    st.title("Resume ML Model")
+    st.caption("Version: " + "0.0.3")
+    st.write("Although we will not store your resume, feel free to mask out any personal information.")
+
+    # Create file uploader
+    uploaded_file = st.file_uploader('Upload a file', type=['pdf'])
+    if uploaded_file is not None:
+        st.session_state.file = uploaded_file
+    if hasattr(st.session_state, 'file'):
+        with st.spinner('Calling API...'):
+            st.session_state.text = extract_resume(uploaded_file)
+            s_result, v_result = analyse(st.session_state.text)
+            st.code('Using a Stacking Classifier (F1 Score = 0.81), the top 3 predictions are:', language=None)
+            st.dataframe(s_result.head(3))
+            for i in range(2): 
+                st.text("")
+            if st.button('Other Models'):
+                st.code('Using a hard Voting Classifier (F1 Score = 0.82), your resume is most suited in the {} field.'.format(v_result[0].title()), language=None)
+
+def page_two():
+    # About Stacking
+    st.title("Stacking Classifier")
+    st.write("The StackingClassifier is a meta-estimator that fits base classifiers each on the whole dataset. It then uses these base classifiers to build a new dataset which is used as the input to a meta-classifier. The meta-classifier is trained on this new dataset.")
+    st.vega_lite_chart(show_importance(), {
+        'mark': 'bar',
+        'height': 300,
+        'encoding': {
+            'y': {'field': 'Model', 'type': 'nominal', 'sort': '-x'},
+            'x': {'field': 'Importance', 'type': 'quantitative', 'axis': {'format': '%'}},
+            'color': {'field': 'Model', 'type': 'nominal', 'legend': None},
+            'tooltip': [{'field': 'Model', 'type': 'nominal'}, {'field': 'Importance', 'type': 'quantitative','format': '.2%'}],
+        },
+    }, use_container_width=True)
+
+    st.subheader('Top 10 Features for each field')
+    with st.spinner('Computing features...'):
+        st.session_state.df_stack, st.session_state.fields = show_dataframe()
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.session_state.field = st.selectbox('', st.session_state.fields)
+        st.dataframe(
+            st.session_state.df_stack[st.session_state.field.upper()]
+            .sort_values(ascending=False).head(10).to_frame()
+            .rename(columns={st.session_state.field.upper(): 'Importance'}), height=400
+        )
+
+    # About Voting
+    for i in range(3): 
+        st.text("")
+    if st.button('Other Models'):
+        st.title('Voting Classifier')
+        st.write("The VotingClassifier is an ensemble-learning meta-classifier for classification which fits base classifiers each on the whole dataset. It, then, aggregates the individual predictions to form a final prediction. The predictions of each classifier are weighted by their accuracy on the given dataset.")
+        st.write("For this model, the weights of the votes are uniform.")
+
+def main():
     st.set_page_config(page_title='Resume Analyzer by Guidesify', layout="wide")
     st.markdown(read_text('./Text/hide_table_sn.txt'), unsafe_allow_html=True)
     st.markdown(read_text('./Text/hide_streamlit_menu.txt'), unsafe_allow_html=True) 
@@ -67,59 +121,12 @@ if __name__ == '__main__':
 
     # Main Page
     if option == 'Main':
-        st.title("Resume ML Model")
-        st.caption("Version: " + "0.0.3")
-        st.write("Although we will not store your resume, feel free to mask out any personal information.")
-
-        # Create file uploader
-        uploaded_file = st.file_uploader('Upload a file', type=['pdf'])
-        if uploaded_file is not None:
-            st.session_state.file = uploaded_file
-        if hasattr(st.session_state, 'file'):
-            with st.spinner('Calling API...'):
-                st.session_state.text = extract_resume(uploaded_file)
-                s_result, v_result = analyse(st.session_state.text)
-                st.code('Using a Stacking Classifier (F1 Score = 0.81), the top 3 predictions are:', language=None)
-                st.dataframe(s_result.head(3))
-                for i in range(2): 
-                    st.text("")
-                if st.button('Other Models'):
-                    st.code('Using a hard Voting Classifier (F1 Score = 0.82), your resume is most suited in the {} field.'.format(v_result[0].title()), language=None)
+        page_one()
     
     # About Page
     elif option == 'About Models':
-        # About Stacking
-        st.title("Stacking Classifier")
-        st.write("The StackingClassifier is a meta-estimator that fits base classifiers each on the whole dataset. It then uses these base classifiers to build a new dataset which is used as the input to a meta-classifier. The meta-classifier is trained on this new dataset.")
-        st.vega_lite_chart(show_importance(), {
-            'mark': 'bar',
-            'height': 300,
-            'encoding': {
-                'y': {'field': 'Model', 'type': 'nominal', 'sort': '-x'},
-                'x': {'field': 'Importance', 'type': 'quantitative', 'axis': {'format': '%'}},
-                'color': {'field': 'Model', 'type': 'nominal', 'legend': None},
-                'tooltip': [{'field': 'Model', 'type': 'nominal'}, {'field': 'Importance', 'type': 'quantitative','format': '.2%'}],
-            },
-        }, use_container_width=True)
+        page_two()
 
-        st.subheader('Top 10 Features for each field')
-        with st.spinner('Computing features...'):
-            st.session_state.df_stack, st.session_state.fields = show_dataframe()
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                st.session_state.field = st.selectbox('', st.session_state.fields)
-            st.dataframe(
-                st.session_state.df_stack[st.session_state.field.upper()]
-                .sort_values(ascending=False).head(10).to_frame()
-                .rename(columns={st.session_state.field.upper(): 'Importance'}), height=400
-            )
-
-        # About Voting
-        for i in range(3): 
-            st.text("")
-        if st.button('Other Models'):
-            st.title('Voting Classifier')
-            st.write("The VotingClassifier is an ensemble-learning meta-classifier for classification which fits base classifiers each on the whole dataset. It, then, aggregates the individual predictions to form a final prediction. The predictions of each classifier are weighted by their accuracy on the given dataset.")
-            st.write("For this model, the weights of the votes are uniform.")
-
-
+# App UI
+if __name__ == '__main__':
+    main()
